8f42e9c9c2d75b6cf160245a39555af6ecf1bd15
pdomokos <domokospeter@gmail.com>
2022-05-19 19:55:51 +0200

    Changes

--- src/main/java/com/example/springboottest/controller/TestController.java
+++ src/main/java/com/example/springboottest/controller/TestController.java
@@ -2,8 +2,12 @@ package com.example.springboottest.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,56 +20,15 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class TestController {
 
-    @Qualifier("httpClient")
-    private final HttpClient httpClient;
+    private final WebClient webClient;
 
-    @Qualifier("httpClient2")
-    private final HttpClient httpClient2;
+    @Value("${test.host}")
+    private String testHost;
 
-
-    @GetMapping(value = "/springboot-httpclient-test")
-    public String getTestUsingHttpClient() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://192.168.0.59:8082/v1/get-test-string"))
-                .build();
-
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
-    }
-
-    @GetMapping(value = "/springboot-httpclient-async-test")
-    public CompletableFuture<String> getTestAsyncUsingHttpClient() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://192.168.0.59:8082/v1/get-test-string-async"))
-                .build();
-
-        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body);
-    }
-
-    @GetMapping(value = "/springboot-httpclient-test-2")
-    public String getTestUsingHttpClient2() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://192.168.0.59:8082/v1/get-test-string"))
-                .build();
-
-        HttpResponse<String> response = httpClient2.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
-    }
-
-    @GetMapping(value = "/springboot-httpclient-async-test-2")
-    public CompletableFuture<String> getTestAsyncUsingHttpClient2() {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://192.168.0.59:8082/v1/get-test-string-async"))
-                .build();
-
-        return httpClient2.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body);
+    @GetMapping(value = "/webflux-webclient-test")
+    public Mono<String> getTestUsingWebfluxWebclient(@RequestParam long delay) {
+        return webClient.get().uri("/v1/get-test-string-1?delay={delay}", delay)
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
--- src/main/java/com/example/springboottest/controller/TestControllerHost.java
+++ src/main/java/com/example/springboottest/controller/TestControllerHost.java
@@ -1,18 +1,17 @@
 package com.example.springboottest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class TestControllerHost {
 
-    @GetMapping("/v1/get-test-string")
-    public String getTest() {
-        return "test string 1";
-    }
-
-    @GetMapping("/v1/get-test-string-async")
-    public String getTestAsync() {
-        return "test string 3";
+    @GetMapping("/v1/get-test-string-1")
+    public Mono<String> getTest1(@RequestParam long delay) {
+        return Mono.just("test string 1").delayElement(Duration.ofMillis(delay));
     }
 }
